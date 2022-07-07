6e04f7041eac329fbff08bbbdea48b94d882ab0e
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-07 10:39:22 +0400

    working on users list

--- todo-hooks/src/App.js
+++ todo-hooks/src/App.js
@@ -1,8 +1,63 @@
-const App = () => {
-  return (
-    <section>
+import { useState } from "react"
+import List from "./Components/List/List"
+import Listitem from "./Components/Listitem/Listitem"
+import Text from "./Components/Text/Text"
+import Wrapper from "./ui/Wrapper/Wrapper"
 
-    </section>
+const App = () => {
+  const [userList,setUserList] = useState([
+    {
+       name: 'Narfioro', 
+       surname: 'Ghazaryan', 
+       age: 32, 
+       hobby: 'coding', 
+       range: 'expert', 
+       id: 1
+    }, 
+    {
+      name: 'Narfi', 
+      surname: 'Xudoyan', 
+      age: 30, 
+      hobby: 'eating', 
+      range: 'expert', 
+      id: 2
+    }, 
+    {
+      name: 'Hamo', 
+      surname: 'Haxverdyan', 
+      age: 22, 
+      hobby: 'coding', 
+      range: 'nub', 
+      id: 3
+    }, 
+    {
+      name: 'Kate', 
+      surname: 'Kalamocew', 
+      age: 28, 
+      hobby: 'coding', 
+      range: 'God level', 
+      id: 4
+    }, 
+  ])
+
+  return (
+    <Wrapper>
+       <List>
+        {
+          userList.map(user => {
+            return (
+              <Listitem key={user.id}>
+                <Text> {user.name} </Text>
+                <Text> {user.surname} </Text>
+                <Text> {user.age} </Text>
+                <Text> {user.hobby} </Text>
+                <Text> {user.range} </Text>
+              </Listitem>
+            )
+          })
+        }
+       </List>
+    </Wrapper>
   )
 }
 
