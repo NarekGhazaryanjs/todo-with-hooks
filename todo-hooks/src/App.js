897a1ee9997c39e0533687c6236b357b1019cb8e
Narek Ghazaryan <narek.ghazaryan@dataart.com>
2022-07-07 11:18:04 +0400

    adding new users to project logic

--- todo-hooks/src/App.js
+++ todo-hooks/src/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import Button from "./Components/Button/Button"
+import Input from "./Components/Input/Input"
 import List from "./Components/List/List"
 import Listitem from "./Components/Listitem/Listitem"
 import Text from "./Components/Text/Text"
+import Card from "./ui/Card/Card"
 import Wrapper from "./ui/Wrapper/Wrapper"
 
 const App = () => {
@@ -41,13 +43,68 @@ const App = () => {
     }, 
   ])
 
+  const newUserName = useRef('');
+  const newUserSurname = useRef('');
+  const newUserAge = useRef('');
+  const newUserHobby = useRef('');
+  const newUserRange = useRef('');
+  const [nextUserId,setNextUserId] = useState(5);
+
+
   const deleteUser = (id) => {
      const newUserList = userList.filter(user => user.id !== id);
      setUserList(newUserList)
   }
 
+  const checkAge = (item) => {
+    return Number(item.current.value) >= 18 && Number(item.current.value) <70
+  }
+
+  const checkInputsValue = (inputsArray) => {
+   const realInputs = inputsArray.filter(el => el.current.value.length >= 2 && el.current.value.length <=10)
+   return realInputs.length === 4
+  }
+
+  const addUser = () => {
+    if(checkInputsValue([newUserName,newUserName,newUserHobby,newUserAge]) 
+      && checkAge(newUserAge)) {
+      const newUser = {
+      name: newUserName.current.value,
+      surname: newUserSurname.current.value,
+      age: newUserAge.current.value,
+      hobby: newUserHobby.current.value,
+      range: newUserRange.current.value,
+      id: nextUserId
+    }
+      setUserList(userList.concat(newUser));
+      setNextUserId(nextUserId + 1)
+    }
+     return
+  }
+
   return (
     <Wrapper>
+      <Card>
+        <Text> new user Name </Text>
+        <Input propsRef={newUserName}  placeholder='write new user name' type='text'/>
+      </Card>
+      <Card>
+        <Text> new user Surname </Text>
+        <Input propsRef={newUserName}  placeholder='write new user surname' type='text'/>
+      </Card>
+      <Card>
+        <Text> new user age </Text>
+        <Input propsRef={newUserAge}  placeholder='write new user age' type='number'/>
+      </Card>
+      <Card>
+        <Text> new user hobby </Text>
+        <Input propsRef={newUserRange}  placeholder='write new user hobby' type='text'/>
+      </Card>
+      <Card>
+        <Text> new user range </Text>
+        <Input propsRef={newUserHobby}  placeholder='write new user range' type='text'/>
+        <Button onClick={addUser}> add user </Button>
+      </Card>
        <List>
         {
           userList.map(user => {
